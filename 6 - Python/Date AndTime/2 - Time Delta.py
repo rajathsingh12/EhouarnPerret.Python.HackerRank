336f61c323d9455c7f89518e494a53917d05386a
Rajath Singh <35859462+rajathsingh12@users.noreply.github.com>
2018-11-17 16:39:08 +0530

    Update 2 - Time Delta.py

--- 6 - Python/Date AndTime/2 - Time Delta.py
+++ 6 - Python/Date AndTime/2 - Time Delta.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-fmt = "%a %d %b %Y %H:%M:%S %z"
+fmt = "%a %d %b %Y %H:%M:%S %z"  # http://strftime.org/ For the format
 T = int(input())
 for _ in range(T):
 	dt1 = datetime.strptime(input(), fmt)
